633ff4880468be0e1c0c511619f7e4c3c2191649
20essentials <s0l0estudi4r@gmail.com>
2024-10-17 10:52:14 +0200

    v0.0.1

---
+++ reference.md
@@ -0,0 +1,3 @@
+- **Reference ⬇️**
+  - [Online Tutorials Video](https://youtu.be/TQVI1fzGjTA?si=yct_OhMkCtEZDgDP)
+  - [Texture Image](https://www.vecteezy.com/vector-art/21584892-seamless-floral-pattern-with-tropical-flowers-and-leaves)
---
+++ script.js
@@ -0,0 +1,10 @@
+console.groupCollapsed('Reference ⬇️');
+console.log(
+  'Online Tutorials Video',
+  'https://youtu.be/TQVI1fzGjTA?si=yct_OhMkCtEZDgDP'
+);
+console.log(
+  'Texture Image',
+  'https://www.vecteezy.com/vector-art/21584892-seamless-floral-pattern-with-tropical-flowers-and-leaves'
+);
+console.groupEnd('');
